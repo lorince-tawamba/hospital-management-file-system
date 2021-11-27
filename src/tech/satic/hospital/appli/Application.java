ff6c7dcffb773eaaccd5123b1108bf631cdf3eae
lorincet <lorincet@gmail.com>
2021-11-27 15:39:19 +0100

    Update FileManagementSystem, isFileExist

--- src/tech/satic/hospital/appli/Application.java
+++ src/tech/satic/hospital/appli/Application.java
@@ -2,7 +2,8 @@
 package tech.satic.hospital.appli;
 
 // Librairie
-import java.util.Arrays;
+
+import tech.satic.hospital.service.FileManagementSystem;
 
 /**
  * @author Lorince TAWAMBA
@@ -11,6 +12,19 @@ import java.util.Arrays;
  */
 public class Application {
     public static void main(String[] args) {
+        FileManagementSystem fileManagementSystem = new FileManagementSystem();
         System.out.println("==== Welcome ====");
+
+        String dirPath = "C:\\Users\\client\\gestion";
+        String fileName = "medecin.txt";
+        boolean state = fileManagementSystem.isFileExist(dirPath, fileName);
+        if (state) {
+            System.out.println("==== The file : " + fileName + " ====");
+            System.out.println("==== exist in path : " + dirPath + " ====");
+        }
+        else {
+            System.out.println("==== The file : " + fileName + " ====");
+            System.out.println("==== does not exist in path : " + dirPath + " ====");
+        }
     }
 }
---
+++ src/tech/satic/hospital/service/FileManagementSystem.java
@@ -0,0 +1,34 @@
+// Package
+package tech.satic.hospital.service;
+
+// Librairie
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * @author Lorince TAWAMBA
+ * @since 27/11/2021
+ * @version 1.0.0
+ */
+
+public class FileManagementSystem {
+    private File file;
+    private String userPath;
+    private String dir;
+
+    public FileManagementSystem() {
+        this.userPath = System.getProperty("user.home");
+        this.file = new File(userPath);
+        this.dir = "gestion";
+    }
+
+    public boolean isFileExist(String dirPath, String fileName) {
+        String separator = System.getProperty("file.separator");
+        String path = dirPath + separator + fileName;
+        this.file = new File(path);
+        return file.exists();
+    }
+
+}
