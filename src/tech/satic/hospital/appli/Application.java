be52a9e509cc59dd0942cebfcf2d7b620392dae8
lorincet <lorincet@gmail.com>
2021-11-27 13:59:49 +0100

    Create main class

---
+++ src/tech/satic/hospital/appli/Application.java
@@ -0,0 +1,15 @@
+// Package
+package tech.satic.hospital.appli;
+
+import java.util.Arrays;
+
+/**
+ * @author Lorince TAWAMBA
+ * @since 27/11/2021
+ * @version 1.0.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("==== Welcome ====");
+    }
+}
